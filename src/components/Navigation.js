4a9a02ee3f97f17ac677fbf1248a549671f32ba9
AnJiIn <aji0719@gmail.com>
2022-01-10 22:52:39 +0900

    Navigation 2

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,25 @@
 import React from "react";
+import hamberger_btn from './img/icon-menu.png';
+import logo_btn from './img/logo.png';
+import './Navigation.css';
 
+const style ={
+  backgroundColor: 'lightblue',
+  height: '16px'
+}
 function Navigation(){
   return(
     <div className="nav-main--container">
-      <div>
-        <button className='nav-main--btn'>Wanted</button>
+      <div className="">
+        <img src={hamberger_btn} alt="hamberger menu" style={style}></img>
+        <img src={logo_btn} alt="logo" style={style}></img>
+
+        <ul className="main-menu toggle">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
       </div>
     </div>
   )
