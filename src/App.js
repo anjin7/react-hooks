e2ac86df302e8490e85f6c4db394b58f60f12d9b
AnJiIn <aji0719@gmail.com>
2022-01-10 22:13:45 +0900

    Navigation 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from "react";
 import './reset.css';
 import './App.css';
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <Navigation></Navigation>
+      </header>
     </div>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navigation(){
+  return(
+    <div className="nav-main--container">
+      <div>
+        <button className='nav-main--btn'>Wanted</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navigation;
