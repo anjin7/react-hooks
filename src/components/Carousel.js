4c5b40d38c25ceffb28ea59911abf22148bf7231
AnJiIn <aji0719@gmail.com>
2022-01-15 18:47:15 +0900

    Carousel 2

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Carousel.css';
 
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "6.jpg", "7.jpg"];
 
+const slideImg = []
+slideImg.src=`./img/${images}`;
 function Carousel(){
   return(
     <div className="carousel--container">
-      <div>slide</div>
+      <div>slideImg</div>
+      <div>
+        <h3>제목</h3>
+        <p>내용</p>
+      </div>
     </div>
   )
 }
