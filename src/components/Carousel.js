0ffc815957e79d380e32cd674f79ce6f95c18d00
AnJiIn <aji0719@gmail.com>
2022-01-16 14:59:40 +0900

    Carousel 3

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -8,10 +8,12 @@ slideImg.src=`./img/${images}`;
 function Carousel(){
   return(
     <div className="carousel--container">
-      <div>slideImg</div>
-      <div>
-        <h3>제목</h3>
-        <p>내용</p>
+      <a href="/events/pre_onboarding_course_6" className="">
+        <img src="https://static.wanted.co.kr/images/banners/1473/41f7b36e.jpg" alt="개발자 되고싶은 분들!?" className="carousel-img" />
+      </a>      
+      <div className="carousel-card--container">
+        <h2>제목</h2>
+        <h3>내용</h3>
       </div>
     </div>
   )
