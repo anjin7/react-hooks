0986015ab9173e9f4edebb087cadcb46b8dfef06
AnJiIn <aji0719@gmail.com>
2022-01-16 19:24:10 +0900

    Carousel 4

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Carousel.css';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "6.jpg", "7.jpg"];
 
@@ -12,9 +14,16 @@ function Carousel(){
         <img src="https://static.wanted.co.kr/images/banners/1473/41f7b36e.jpg" alt="개발자 되고싶은 분들!?" className="carousel-img" />
       </a>      
       <div className="carousel-card--container">
-        <h2>제목</h2>
-        <h3>내용</h3>
+        <h2>개발자 되고싶은 분들?!</h2>
+        <h3>프론트엔드 무료 교육과정 참여하기</h3>
+        <hr />
+        <a href="/events/pre_onboarding_course_6" className="">
+          <span>바로가기</span>
+        </a>
       </div>
+
+    <button type="button" className="prev_arrow"><ArrowBackIosNewIcon fontSize="small" /></button>
+    <button type="button" className="next_arrow"><ArrowForwardIosIcon fontSize="small" /></button>
     </div>
   )
 }
