2b735c04222764d563002a54f3ae408bd927643a
AnJiIn <aji0719@gmail.com>
2022-01-19 03:40:15 +0900

    Carousel 7

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,54 +4,78 @@ import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 
+const slide = [
+{
+  'id': 1,
+  'href': "/events/pre_onboarding_course_6",
+  'img': "https://static.wanted.co.kr/images/banners/1473/41f7b36e.jpg",
+  'alt': "개발자 되고싶은 분들!?",
+  'h3': "프론트엔드 무료 교육과정 참여하기",
+},
+{
+  'id': 2,
+  'href': "/events/pre_onboarding_course_7",
+  'img': "https://static.wanted.co.kr/images/banners/1490/0b775035.jpg",
+  'alt': "마케팅 주니어를 찾습니다",
+  'h3': "기업 과제 풀고 취업까지 한번에!",
+},
+{
+  'id': 3,
+  'href': "/events/velog01",
+  'img': "https://static.wanted.co.kr/images/banners/1460/619f3af7.jpg",
+  'alt': "개발자 성장 비결 공개!",
+  'h3': "Velog, 글 쓰는 개발자들의 이야기",
+},
+];
+
 function Carousel(){
   return(
     <div className="banner">
       <div className="carousel--window">
         <div className="carousel--list">
-          <div className="carousel slide1">
-            <a href="/events/pre_onboarding_course_6">
-              <img src="https://static.wanted.co.kr/images/banners/1473/41f7b36e.jpg" alt="개발자 되고싶은 분들!?" className="carousel-img" />
+          <div className="carousel" id={slide[0].id}>
+            <a href={slide[0].href}>
+              <img src={slide[0].img} alt={slide[0].alt} className="carousel-img" />
             </a>
             <div className="carousel-card--container">
-              <h2>개발자 되고싶은 분들?!</h2>
-              <h3>프론트엔드 무료 교육과정 참여하기</h3>
+              <h2>{slide[0].alt}</h2>
+              <h3>{slide[0].h3}</h3>
               <hr />
-              <a href="/events/pre_onboarding_course_6" className="carousel-card--link">
+              <a href={slide[0].href} className="carousel-card--link">
                 <span>바로가기</span>
               </a>
             </div>
           </div>
-          <div className="carousel slide2">
-            <a href="/events/pre_onboarding_course_7">
-              <img src="https://static.wanted.co.kr/images/banners/1490/0b775035.jpg" alt="마케팅 주니어를 찾습니다" className="carousel-img" />
+          <div className="carousel show" id={slide[1].id}>
+            <a href={slide[1].href}>
+              <img src={slide[1].img} alt={slide[1].alt} className="carousel-img" />
             </a>
             <div className="carousel-card--container">
-              <h2>마케팅 주니어를 찾습니다</h2>
-              <h3>기업 과제 풀고 취업까지 한번에!</h3>
+              <h2>{slide[1].alt}</h2>
+              <h3>{slide[1].h3}</h3>
               <hr />
-              <a href="/events/pre_onboarding_course_7" className="carousel-card--link">
+              <a href={slide[1].href} className="carousel-card--link">
                 <span>바로가기</span>
               </a>
             </div>
           </div>
-          <div className="carousel slide3">
-            <a href="/events/velog01" class="" aria-label="">
-              <img src="https://static.wanted.co.kr/images/banners/1460/619f3af7.jpg" alt="개발자 성장 비결 공개!" className="carousel-img" />
+          <div className="carousel" id={slide[2].id}>
+            <a href={slide[2].href}>
+              <img src={slide[2].img} alt={slide[2].alt} className="carousel-img" />
             </a>
             <div className="carousel-card--container">
-              <h2>개발자 성장 비결 공개!</h2>
-              <h3>Velog, 글 쓰는 개발자들의 이야기</h3>
+              <h2>{slide[2].alt}</h2>
+              <h3>{slide[2].h3}</h3>
               <hr />
-              <a href="/events/velog01" className="carousel-card--link">
+              <a href={slide[2].href} className="carousel-card--link">
                 <span>바로가기</span>
               </a>
             </div>
-          </div>                  
+          </div>
         </div>        
       </div>
-      <button type="button" className="prev_arrow"><ArrowBackIosNewIcon fontSize="small" /></button>
-      <button type="button" className="next_arrow"><ArrowForwardIosIcon fontSize="small" /></button>
+      <button type="button"  className="prevBtn"><ArrowBackIosNewIcon fontSize="small" /></button>
+      <button type="button"  className="nextBtn"><ArrowForwardIosIcon fontSize="small" /></button>
     </div>
   )
 }
