70cb244b054175e5245c860beae0dda922090787
_An_Jin_ <aji0719@gmail.com>
2022-08-01 22:55:50 +0900

    reset

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Carousel (연습)
-
-https://awesome-swartz-39eb1c.netlify.app
+# React Hooks
--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from "react";
 import './reset.css';
 import './App.css';
-import Navigation from "./components/Navigation";
-import Carousel from './components/Carousel';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <Navigation />
-      </header>
-      <section>
-        <Carousel />
-      </section>        
+      Hello
     </div>
   );
 }
